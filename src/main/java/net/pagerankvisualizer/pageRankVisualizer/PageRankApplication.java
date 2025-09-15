a1ce68b73008726781287c768b353d50aeda73b4
anshul755 <anshulpatel2023@gmail.com>
2025-09-16 02:29:51 +0530

    Initial commit

---
+++ src/main/java/net/pagerankvisualizer/pageRankVisualizer/PageRankApplication.java
@@ -0,0 +1,15 @@
+package net.pagerankvisualizer.pageRankVisualizer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageRankApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/net/pagerankvisualizer/pageRankVisualizer/PageRankApplicationTest.java
@@ -0,0 +1,13 @@
+package net.pagerankvisualizer.pageRankVisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageRankApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
